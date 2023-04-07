f89c7936a5a4703de6e8fc5403441c092a4bdfdf
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-04-07 16:59:14 +0200

    Draft: Add gem5 IO

---
+++ gem5/io/src/lib.rs
@@ -0,0 +1,168 @@
+#![no_std]
+
+use core::arch::asm;
+
+use io_trait::InputOutput;
+
+static mut WRITE_BUF: [u8; 16] = [0; 16];
+static mut READ_BUF: [u8; 16] = [0; 16];
+
+#[inline]
+fn hex_to_nible(h: u8) -> u8 {
+    (((h & 0x40) >> 6) * 9) + (h & 0xF)
+}
+
+static HEX_TABLE: [u8; 16] = [
+    b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'A', b'B', b'C', b'D', b'E', b'F',
+];
+
+pub struct IO;
+
+impl InputOutput for IO {
+    #[inline]
+    fn start() {
+        for b in b"*** START\n" {
+            Self::write_byte(*b);
+        }
+    }
+
+    #[inline]
+    fn read_byte() -> u8 {
+        loop {
+            let mut size: u32;
+            unsafe {
+                asm!(
+                    "ecall",
+                    inout ("a7") 63 => _,
+                    inout ("a0") 0 => size,
+                    inout ("a1") &READ_BUF as *const u8 => _,
+                    inout ("a2") 1 => _,
+                    options(nostack),
+                );
+            }
+
+            if size == 1 {
+                break;
+            }
+        }
+
+        unsafe { READ_BUF[0] }
+    }
+
+    #[inline]
+    fn write_byte(b: u8) {
+        unsafe {
+            WRITE_BUF[0] = b;
+        }
+
+        loop {
+            let mut size: u32;
+            unsafe {
+                asm!(
+                    "ecall",
+                    inout ("a7") 64 => _,
+                    inout ("a0") 1 => size,
+                    inout ("a1") &WRITE_BUF as *const u8 => _,
+                    inout ("a2") 1 => _,
+                    options(nostack),
+                );
+            }
+
+            if size == 1 {
+                break;
+            }
+        }
+    }
+
+    fn read_word() -> u32 {
+        let b1 = loop {
+            match Self::read_byte() {
+                0 => continue,
+                b => break b,
+            }
+        };
+
+        loop {
+            let mut size: u32;
+            unsafe {
+                asm!(
+                    "ecall",
+                    inout ("a7") 63 => _,
+                    inout ("a0") 0 => size,
+                    inout ("a1") &READ_BUF as *const u8 => _,
+                    inout ("a2") 7 => _,
+                    options(nostack),
+                );
+            }
+
+            if size == 7 {
+                break;
+            }
+        }
+
+        unsafe {
+            return ((hex_to_nible(b1) as u32) << 28)
+                | ((hex_to_nible(READ_BUF[0]) as u32) << 24)
+                | ((hex_to_nible(READ_BUF[1]) as u32) << 20)
+                | ((hex_to_nible(READ_BUF[2]) as u32) << 16)
+                | ((hex_to_nible(READ_BUF[3]) as u32) << 12)
+                | ((hex_to_nible(READ_BUF[4]) as u32) << 8)
+                | ((hex_to_nible(READ_BUF[5]) as u32) << 4)
+                | ((hex_to_nible(READ_BUF[6]) as u32) << 0);
+        }
+    }
+
+    fn write_word(w: u32) {
+        unsafe {
+            WRITE_BUF[0] = HEX_TABLE[(w.overflowing_shr(28).0 & 0xF) as usize];
+            WRITE_BUF[1] = HEX_TABLE[(w.overflowing_shr(24).0 & 0xF) as usize];
+            WRITE_BUF[2] = HEX_TABLE[(w.overflowing_shr(20).0 & 0xF) as usize];
+            WRITE_BUF[3] = HEX_TABLE[(w.overflowing_shr(16).0 & 0xF) as usize];
+            WRITE_BUF[4] = HEX_TABLE[(w.overflowing_shr(12).0 & 0xF) as usize];
+            WRITE_BUF[5] = HEX_TABLE[(w.overflowing_shr(8).0 & 0xF) as usize];
+            WRITE_BUF[6] = HEX_TABLE[(w.overflowing_shr(4).0 & 0xF) as usize];
+            WRITE_BUF[7] = HEX_TABLE[(w.overflowing_shr(0).0 & 0xF) as usize];
+            WRITE_BUF[8] = b'\n';
+        }
+
+        loop {
+            let mut size: u32;
+
+            unsafe {
+                asm!(
+                    "ecall",
+                    inout ("a7") 64 => _,
+                    inout ("a0") 1 => size,
+                    inout ("a1") &WRITE_BUF as *const u8 => _,
+                    inout ("a2") 9 => _,
+                    options(nostack),
+                );
+            }
+
+            if size == 9 {
+                break;
+            }
+        }
+    }
+
+    fn time_addr_read(addr: *const u32) -> u32 {
+        let value;
+
+        unsafe {
+            asm!(
+                r#"
+                    rdcycle {duration}
+                    lw      {loaded_value},0({addr}) 
+                    rdcycle {after}
+                    sub     {duration},{after},{duration}
+                "#,
+                addr = inout (reg) addr => _,
+                duration = out (reg) value,
+                loaded_value = out (reg) _,
+                after = out (reg) _,
+            )
+        }
+
+        value
+    }
+}
---
+++ python-srcs/gem5_io.py
@@ -0,0 +1,72 @@
+from time import sleep
+from io_trait import TargetIO
+
+START_LINE = "*** START"
+END_LINE = "*** END"
+
+HEX_TABLE = [
+    ord("0"),
+    ord("1"),
+    ord("2"),
+    ord("3"),
+    ord("4"),
+    ord("5"),
+    ord("6"),
+    ord("7"),
+    ord("8"),
+    ord("9"),
+    ord("A"),
+    ord("B"),
+    ord("C"),
+    ord("D"),
+    ord("E"),
+    ord("F"),
+]
+
+
+class Gem5IO(TargetIO):
+    def __init__(self, in_file, out_file) -> None:
+        super()
+
+        self.in_file_path = in_file
+        self.out_file_path = out_file
+
+        self.start_line = None
+        while self.start_line == None:
+            out_file = open(self.out_file_path, "r")
+            lines = out_file.readlines()
+            out_file.close()
+
+            self.start_line = next(
+                (i for i, line in list(enumerate(lines)) if START_LINE in line), None
+            )
+
+            sleep(0.1)
+
+        self.offset_line = self.start_line + 1
+
+    def send_word(self, w):
+        bs = "{:08X}".format(w).encode()
+        bs = bytearray(bs)
+        bs.append(0)
+
+        in_file = open(self.in_file_path, "ab")
+        in_file.write(bs)
+        in_file.close()
+
+    def receive_word(self):
+        while True:
+            out_file = open(self.out_file_path, "r")
+            lines = out_file.readlines()
+            out_file.close()
+
+            # If the `\n` is added to the end of the line
+            if self.offset_line < len(lines) and lines[self.offset_line].endswith('\n'):
+                break
+
+        line = lines[self.offset_line].strip()
+        assert len(line) == 8
+
+        self.offset_line += 1
+
+        return int(line, 16)
