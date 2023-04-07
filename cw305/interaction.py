89d5f138bdd2334cb01184313189ce96caf79ae3
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-04-07 17:03:46 +0200

    Draft: Add cw305 run files

---
+++ cw305/interaction.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+from cw305_io import CW305IO
+import find_sets
+# import io_accuracy
+from program import RAM
+from connection import PulpinoConnection
+
+bitpath = "./set_associative_cache.bit"
+pulpino = PulpinoConnection(bitpath, force = True)
+
+if not pulpino.get_raw().fpga.isFPGAProgrammed():
+    print("Fpga Not Programmed")
+    exit(1)
+
+pulpino.reset()
+pulpino.program(0x0, RAM)
+
+# Stop Programming
+pulpino.stop_programming()
+
+# Entry Address
+pulpino.send_word(0x0)
+
+cw305 = CW305IO(pulpino)
+
+find_sets.run(cw305)
+# io_accuracy.run(cw305)
---
+++ cw305/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ATT_PATH="../att"
+
+if [ -z $1 ] ; then
+    echo "Usage: $0 <path/to/attack/dir>"
+    exit 2
+fi
+
+ATT_DIR="$1"
+ATT_BIN="$ATT_PATH/$ATT_DIR/target/riscv32i-unknown-none-elf/release/$ATT_DIR"
+
+set -e
+
+pushd "$ATT_PATH/$ATT_DIR" > /dev/null
+cargo build --release --no-default-features --features cw305
+popd > /dev/null
+
+riscv-none-elf-objdump -D "$ATT_BIN" > dump.out
+./to_ram.py dump.out program.py
+./interaction.py
